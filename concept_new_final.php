d4662c4e081ec0327858bbffadc482a06ea7c1bc
cilo <migayicecil@gmail.com>
2017-04-17 16:22:55 +0300

    more updates

--- concept_new_final.php
+++ concept_new_final.php
@@ -88,6 +88,45 @@ $(document).ready(function(){
 		$('.invite-items').css("display","none");
 		$("input[name='type']:not(checked)").removeAttr('disabled');
 	});
+    
+    $('.textarea').on('keyup',function(){
+        var charCount = $(this).text().length;
+        $(".result").text(charCount + " chars");
+	});
+    
+     $('.textarea').click(function(){
+         $('.textarea-menu').fadeIn(1000).css("dispaly","block");
+     });
+    
+    $('.textarea img').mousedown(function(event){            
+        event.preventDefault();
+        if(event.which == 3){
+            alert("wow");
+        }
+    });
+    
+    $('.textarea').contextmenu(function() {
+        return false;
+    });
+    
+    $('.menu-img').click(function(){
+        $('.textarea-menu-img').fadeIn(1000).css("display","block");
+        $('.textarea-menu-main').fadeOut(1000).css("display","none");
+    });
+    $('.menu-link').click(function(){
+        $('.textarea-menu-link').fadeIn(1000).css("display","block");
+        $('.textarea-menu-main').fadeOut(1000).css("display","none");
+    });
+    $('.menu-font').click(function(){
+        $('.textarea-menu-font').fadeIn(1000).css("display","block");
+        $('.textarea-menu-main').fadeOut(1000).css("display","none");
+    });
+    $('.close-btn').click(function(){
+         $('.textarea-menu-main').fadeIn(1000).css("display","block");
+         $('.textarea-menu-img').fadeOut(1000).css("display","none");
+         $('.textarea-menu-link').fadeOut(1000).css("display","none");
+         $('.textarea-menu-font').fadeOut(1000).css("display","none");
+    });
 });
 </script>
 <style>
@@ -105,11 +144,22 @@ $(document).ready(function(){
 	border-radius:3px;
 	border:#ddd solid 0.1em;
 }
-.new-concept-content form textarea{
+.new-concept-content form .textarea{
 	width:100%;
 	padding:1%;
 	border-radius:3px;
 	border:#ddd solid 0.1em;
+    -moz-appearance: textfield-multiline;
+    -webkit-appearance: textarea;
+    font: medium -moz-fixed;
+    font: -webkit-small-control;
+    height: 118px;
+    overflow: auto;
+    resize: both;
+    background: #fff;
+}
+.textarea img{
+    cursor:pointer;
 }
 .new-concept-content form select{
 	width:100%;
@@ -170,6 +220,40 @@ $(document).ready(function(){
 .text-post{display:none;}
 .video-post{display:none;}
 .audio-post{display:none;}
+.textarea-menu{
+    display: none;
+    margin-top:3%;
+}
+.textarea-menu ul{
+    padding: 0px;
+    margin: 0px;
+    list-style: none;
+}
+.textarea-menu li{
+    display: inline;
+    width:10%;
+    border:#ccc solid 0.1em;
+    padding:1%;
+}
+.textarea-menu li img{
+    width:3%;
+}
+.textarea-menu-main{
+    display:block;
+    width:100%;
+}
+.textarea-menu-img{
+    display:none;
+    width:100%;
+}
+.textarea-menu-link{
+    display:none;
+    width:100%  
+}
+.textarea-menu-font{
+    display:none;
+    width:100% 
+}
 </style>
 <div class="new-concept-content">
 	<h3>Final Stage: Posting a new concept</h3>
@@ -196,7 +280,42 @@ $(document).ready(function(){
 			<div class="info">Select content type above to continue...</div>
 			<div class="text-post">	
 				Write your content in the text area below:
-				<textarea name="concept_content"></textarea>
+				<div name="concept_content" class="textarea" 
+                     contenteditable></div>
+                <div class="result"></div>
+                <div class="textarea-menu">
+                    <div class="textarea-menu-main">
+                        <ul>
+                            <li class="menu-img"><img src="img/camera.png"/> add image</li>
+                            <li class="menu-link">Add link</li>
+                            <li class="menu-font">change font</li>
+                        </ul>
+                    </div> 
+                    <div class="textarea-menu-img">
+                        <a href="#" class="close-btn">X Close</a>
+                        <form>
+                            <input type="file" name="file"/>
+                        </form>
+                    </div>
+                    <div class="textarea-menu-link">
+                        <a href="#" class="close-btn">X Close</a>
+                        <form>
+                            <input name="link" type="text"/>
+                        </form>
+                    </div>
+                    <div class="textarea-menu-font">
+                        <a href="#" class="close-btn">X Close</a>
+                        <form>
+                            <select>
+                                <option value="arial">arial</option>
+                                <option value="verdana">verdana</option>
+                                <option value="Comic Sans MS">comic sans</option>
+                                <option value="Mavenpro">Mavenpro</option>
+                            </select>
+                            <input name="" placeholder="font size"/>
+                        </form>
+                    </div>
+                </div>
 			</div>
 			<div class="video-post">
 				Choose video file:
@@ -209,7 +328,7 @@ $(document).ready(function(){
 		</div>
 		<div class="new-concept-content-item">
 			<input name="concept_id" type="hidden" value="<?php echo $userConceptData['concept_id'];?>"/>			
-			<input name="submit" type="submit" value="Submit"/>			
+			<input name="submit" type="submit" value="Submit" disabled="disabled">			
 		</div>
 	</form>
 </div>
